b8684f2661e4fb5786651e4c9bfa5c1a066e74df
abhijit <kamathabhijit2002@gmail.com>
2023-11-28 02:03:54 -0500

    set up docker image and cleaned up some parts

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
-FROM node:20
+FROM node:20-alpine
 
+# create the app directory for the container
 WORKDIR /app
 
+# installing all the dependencies and to cache them if no changes
 COPY package*.json ./
 
+# run to install
 RUN npm install 
 
+# bundle the app source
 COPY . .
 
 ENV PORT=5000
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
 
-console.log(process.env.API_KEY);
-console.log(process.env.API_URL);
 const app = express();
 const SB = createClient(process.env.API_URL, process.env.API_KEY);
 
